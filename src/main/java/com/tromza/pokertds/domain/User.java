9781d154ec5f2a590368204ae874ded48fe55584
ViktarTromza <viktar.tromza@gmail.com>
2023-03-29 18:33:26 +0300

    Hibernate begin

--- src/main/java/com/tromza/pokertds/domain/User.java
+++ src/main/java/com/tromza/pokertds/domain/User.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -11,25 +13,41 @@ import java.sql.Date;
 
 @Component
 @Data
+@Entity
+@Table(name = "users")
+@SecondaryTables({
+        @SecondaryTable( name = "users_data"),
+        @SecondaryTable( name="wallets"),})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "login")
     @Pattern(regexp = "[A-z0-9_-]*")
     private String login;
     @Size(min = 5)
+    @Column(name = "password")
     //@JsonIgnore
     private String password;
+    @Column(name = "registration_date")
     private Date regDate;
     @Email
+    @Column(name = "email")
     private String email;
+    @Column(name = "score")
     private int score;
-
+@Column(table = "users_data",name = "first_name")
     private String firstName;
+    @Column(table = "users_data",name = "last_name")
     private String lastName;
+    @Column(table = "users_data",name = "country")
     private String country;
+    @Column(table = "users_data",name = "date_of_birth")
     private String birthDay;
+    @Column(table = "users_data",name = "phone_number")
     private String telephone;
 
-
+@Column(table = "wallets")
     private Wallet wallet;
 
     public User() {
